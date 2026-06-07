616b435a29576484fdb72374812630aba9023326
Eoin Power <24438081@studentmail.ul.ie>
2026-06-07 22:12:27 +0100

    feat: javafx added

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,6 @@
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,50 +10,67 @@ class Game {
     List<String> playerOneHand = deck.getPlayerOneHand();
     List<String> playerTwoHand = deck.getPlayerTwoHand();
     List<String> cardPile = deck.pile;
+    public boolean gameOver = false;
 
-    Player playerOne = new Player("Stephen", playerOneHand);
-    Player playerTwo = new Player("Eoin", playerTwoHand);
+    Player playerOne = new Player("Player One", playerOneHand);
+    Player playerTwo = new Player("Player Two", playerTwoHand);
 
-    int round_counter = 1;
+    int round_counter = 0;
 
     public void startGame(){
+        gameOver = false;
         System.out.println("This is the card game called War!");
 
         deck.createDeck();
         deck.shuffleDeck(deck.deck);
         deck.dealCards(deck.shuffledDeck);
 
-        boolean playing = true;
-        int playerOneHasCards = playerOneHand.size();
-        int playerTwoHasCards = playerTwoHand.size();
-
-
-        round(cardPile);
-
+        //round(cardPile);
     }
 
-    public void round(List<String> cardPile) {
+    public String round(Label card1, Label card2, Text p1Cards, Text p2Cards, Text roundResult) {
 
-        while (!playerOneHand.isEmpty() && !playerTwoHand.isEmpty()) {
-            cardPile.clear();
-            String p1Card = deck.getPlayerOneCard();
-            String p2Card = deck.getPlayerTwoCard();
-
-            cardPile.add(p1Card);
-            cardPile.add(p2Card);
-            compareCard(p1Card, p2Card);
+        if (gameOver) {
+            return "Game Over";
         }
 
         if (playerOneHand.isEmpty()){
+            gameOver = true;
             System.out.println(playerOne.getName() + " has no cards left. " + playerTwo.getName() + " wins!");
-
-        } else {
-            System.out.println(playerTwo.getName() + " has no cards left. " + playerOne.getName() + " wins!");
-
+            return playerOne.getName() + " has no cards left. " + playerTwo.getName() + " wins!";
         }
+
+        if (playerTwoHand.isEmpty()){
+            gameOver = true;
+            System.out.println(playerTwo.getName() + " has no cards left. " + playerOne.getName() + " wins!");
+            return playerTwo.getName() + " has no cards left. " + playerOne.getName() + " wins!";
+        }
+
+        cardPile.clear();
+        String p1Card = deck.getPlayerOneCard();
+        String p2Card = deck.getPlayerTwoCard();
+
+        card1.setText(p1Card);
+        card2.setText(p2Card);
+
+        String num1 = String.valueOf(playerOne.getNumberOfCards());
+        String num2 = String.valueOf(playerTwo.getNumberOfCards());
+        p1Cards.setText("Player 1 Cards: " + num1);
+        p2Cards.setText("Player 2 Cards: " + num2);
+        System.out.println("Player 1 Cards: " + num1);
+        System.out.println("Player 2 Cards: " + num2);
+
+        cardPile.add(p1Card);
+        cardPile.add(p2Card);
+
+        String rResult = compareCard(p1Card, p2Card);
+        roundResult.setText("Round Result: " + rResult);
+
+        return "Round: " + round_counter;
     }
 
-    public void compareCard(String playerOneCard, String playerTwoCard) {
+    public String compareCard(String playerOneCard, String playerTwoCard) {
+        String response = " ";
         int playerOneValue = deck.getCardValue(playerOneCard);
         int playerTwoValue = deck.getCardValue(playerTwoCard);
 
@@ -58,49 +78,61 @@ class Game {
         System.out.println(playerTwo.getName() + "'s" + " card value: " + deck.getCardValue(playerTwoCard));
 
         if (playerOneValue == playerTwoValue) {
-            handleWar();
+            response = "Going to War! " + handleWar();
         } else if (playerOneValue > playerTwoValue) {
-            System.out.println(playerOne.getName() + " wins round " + round_counter);
+            //System.out.println(playerOne.getName() + " wins round " + round_counter);
+            response = playerOne.getName() + " wins!";
             Collections.shuffle(cardPile);
             playerOneHand.addAll(cardPile);
             cardPile.clear();
             round_counter++;
         } else {
-            System.out.println(playerTwo.getName() + " wins round " + round_counter);
+            //System.out.println(playerTwo.getName() + " wins round " + round_counter);
+            response = playerTwo.getName() + " wins!";
             Collections.shuffle(cardPile);
             playerTwoHand.addAll(cardPile);
             cardPile.clear();
             round_counter++;
         }
+        return response;
     }
 
-    public void handleWar() {
+    public String handleWar() {
+        String result = " ";
         System.out.println("WAR");
 
         if (playerOne.getNumberOfCards() < 4 && playerTwo.getNumberOfCards() < 4){
-            System.out.println("not enough cards to play war");
-            System.exit(0);
+            gameOver = true;
+            return "Not enough cards to play War!";
         }
 
         if (playerOne.getNumberOfCards() < 4 && playerTwo.getNumberOfCards() >=4) {
-            System.out.println(playerOne.getName() + " has no cards. " + playerTwo.getName() + " wins!");
-            // player 2 wins the pile
             playerTwoHand.addAll(cardPile);
-        } else if (playerOne.getNumberOfCards() >= 4 && playerTwo.getNumberOfCards() < 4) {
-            System.out.println(playerTwo.getName() + " has no cards. " + playerOne.getName() + " wins!");
-            //player 1 wins the pile
-            playerOneHand.addAll(cardPile);
-        } else {
-            for (int i = 0; i < 3; i++) {
-                cardPile.add(playerOne.getFirstCard());
-                cardPile.add(playerTwo.getFirstCard());
-            }
-            String p1WarCard = playerOne.getFirstCard();
-            String p2WarCard = playerTwo.getFirstCard();
-            cardPile.add(p1WarCard);
-            cardPile.add(p2WarCard);
-            compareCard(p1WarCard, p2WarCard);
+            cardPile.clear();
+            gameOver = true;
+            return playerOne.getName() + " insufficient cards. " + playerTwo.getName() + " wins!";
         }
 
+        if (playerOne.getNumberOfCards() >= 4 && playerTwo.getNumberOfCards() < 4) {
+            playerOneHand.addAll(cardPile);
+            cardPile.clear();
+            gameOver = true;
+            return playerTwo.getName() + " insufficient cards. " + playerOne.getName() + " wins!";
+        }
+
+        for (int i = 0; i < 3; i++) {
+            cardPile.add(playerOne.getFirstCard());
+            cardPile.add(playerTwo.getFirstCard());
+        }
+        String p1WarCard = playerOne.getFirstCard();
+        String p2WarCard = playerTwo.getFirstCard();
+        cardPile.add(p1WarCard);
+        cardPile.add(p2WarCard);
+
+        return compareCard(p1WarCard, p2WarCard);
+}
+
+    public boolean isGameOver() {
+        return gameOver;
     }
 }
--- src/main/java/War.java
+++ src/main/java/War.java
@@ -1,32 +1,128 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import javax.swing.*;
 
 public class War extends Application {
 
     @Override
     public void start(Stage stage) {
+        Game game = new Game();
+
         Button startGame = new Button("Start Game");
+        startGame.setStyle("-fx-background-color: red; -fx-text-fill: Black; -fx-border-color: black; -fx-font-style: bold; -fx-font-family: 'Bookman Old Style'" );
+
+        Rectangle r1 = new Rectangle(110, 170);
+        r1.setFill(Color.WHITE);
+        r1.setStroke(Color.BLACK);
+        r1.setArcWidth(15);
+        r1.setArcHeight(15);
+
+        Rectangle r2 = new Rectangle(110, 170);
+        r2.setFill(Color.WHITE);
+        r2.setStroke(Color.BLACK);
+        r2.setArcWidth(15);
+        r2.setArcHeight(15);
+
+        Label l1 = new Label("Player 1");
+        l1.setStyle("-fx-font-weight: bold; -fx-font-size: 18px; -fx-font-family: 'Bookman Old Style';");
+
+        Label l2 = new Label("Player 2");
+        l2.setStyle("-fx-font-weight: bold; -fx-font-size: 18px; -fx-font-family: 'Bookman Old Style';");
 
 
-        BorderPane pane =  new BorderPane();
+        Label card1 = new Label("Card 1");
+        card1.setStyle("-fx-font-weight: bold;");
 
-        VBox vbox = new VBox(startGame);
+        Label card2 = new Label("Card 2");
+        card2.setStyle("-fx-font-weight: bold;");
+
+        StackPane stackPane1 = new StackPane();
+        stackPane1.getChildren().addAll(r1, card1);
+
+        StackPane stackPane2 = new StackPane();
+        stackPane2.getChildren().addAll(r2, card2);
+
+        Text name = new Text("War Card Game");
+        name.setStyle("-fx-font-weight: bold; -fx-font-family: 'Bookman Old Style'; -fx-font-size: 24px;");
+
+        Text round = new Text("Round Counter:");
+        round.setStyle("-fx-font-weight: bold; -fx-font-family: 'Bookman Old Style'; -fx-font-size: 14px;");
+
+        Text p1Cards = new Text("Player 1 Cards:");
+        p1Cards.setStyle("-fx-font-weight: bold; -fx-font-family: 'Bookman Old Style'; -fx-font-size: 14px;");
+
+        Text p2Cards = new Text("Player 2 Cards:");
+        p2Cards.setStyle("-fx-font-weight: bold; -fx-font-family: 'Bookman Old Style'; -fx-font-size: 14px;");
+
+        Text roundResult = new Text("Round Result:");
+        roundResult.setStyle("-fx-font-weight: bold; -fx-font-family: 'Bookman Old Style'; -fx-font-size: 14px");
+
+        BorderPane pane = new BorderPane();
+
+        HBox hbox = new HBox();
+        hbox.setAlignment(Pos.CENTER);
+        hbox.getChildren().addAll(name);
+        hbox.setPadding(new Insets(40, 0, 0, 0));
+
+        HBox tBox = new HBox(190);
+        tBox.getChildren().addAll(l1, l2);
+        tBox.setAlignment(Pos.CENTER);
+        tBox.setPadding(new Insets(10,0,0,0));
+
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(name, round, p1Cards, p2Cards, roundResult, tBox);
+        pane.setTop(vbox);
         vbox.setAlignment(Pos.CENTER);
-        vbox.setTranslateY(70);
 
-        pane.setCenter(vbox);
+        HBox cardBox = new HBox(150);
+        cardBox.getChildren().addAll(stackPane1, stackPane2);
+        cardBox.setAlignment(Pos.CENTER);
+        cardBox.setPadding(new Insets(0, 0, 0, 0));
+        pane.setCenter(cardBox);
+
+        HBox bBox = new HBox();
+        bBox.setAlignment(Pos.CENTER);
+        bBox.getChildren().addAll(startGame);
+        bBox.setPadding(new Insets(0, 0, 40, 0));
+        pane.setBottom(bBox);
+
+        startGame.setOnAction(e -> {
+           game.startGame();
+           Timeline timeline = new Timeline();
+           timeline.getKeyFrames().add(
+                   new KeyFrame(Duration.seconds(0.2), ex -> {
+                       String result = game.round(card1, card2, p1Cards, p2Cards, roundResult);
+
+                       round.setText(result);
+
+                       if (game.isGameOver()) {
+                           timeline.stop();
+                           startGame.setDisable(true);
+                   }
+           })
+           );
+
+           timeline.setCycleCount(Timeline.INDEFINITE);
+           timeline.play();
+        });
 
 
-        TextArea textArea = new TextArea();
-        textArea.setEditable(false);
-        textArea.setWrapText(true);
-        pane.setBottom(textArea);
 
 
         Scene scene = new Scene(pane, 600, 400);
@@ -34,9 +130,6 @@ public class War extends Application {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
-
-        Game game = new Game();
-        game.startGame();
     }
 
     public static void main(String[] args) {
